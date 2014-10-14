918db8d898f78ba4c3674edddaedc9ec950258bf
ks7000 <jimmyolano@hotmail.com>
2014-10-14 16:12:49 -0430

    Cuestiones de estilo, explica claramente que hace cada decorador

--- multiplica_y_suma.py
+++ multiplica_y_suma.py
@@ -1,31 +1,41 @@
 # -*-encoding:utf-8 -*-
 def sumar(fun):
     def _sumar(*args, **kwargs):
+        print "-----------"
+        print " Función ",fun.__name__,"(",
+        k = 1        
+        for i in args:
+            print i,
+            if k < len(args):
+                print ",",
+            k += 1
+        print ")"
         iterador = 0
         for i in args:
-            iterador += i
+            if isinstance(i,int):            
+                iterador += i
+            else:
+                print "(argumento no válido, se obvia :",i,")"
         fun(*args, **kwargs)
         print "Suma de las multiplicandos:", iterador
+        print
     return _sumar
 
 @sumar
 def multiplicar(*argumentos, **llaves):
     r = 1
     for i in argumentos:
-        r = r * i
+        if isinstance(i,int):        
+            r = r * i
     print "Multiplicando da:",r
 
 @sumar
 def mostrar(*argumentos, **llaves):
     k = 1    
     for i in argumentos:
-        print "Mostrando y sumando: item N°", k, " valor:", i
-        k +=1
+        if isinstance(i,int):        
+            print "Mostrando y sumando: item N°", k, " valor:", i
+            k +=1
 
-print "-----------"
-print "Función 'mostrar(7,11)'"
-mostrar( 7, 11)
-print
-print "-----------"
-print "Función 'multiplicar( 7, 11)'"
-multiplicar( 7, 11)
+mostrar( 7, "Hola", 0.23, 12, 20)
+multiplicar( 7, "Hola", 0.23, 12, 20)
