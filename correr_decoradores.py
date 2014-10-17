e2186dc73c33113c9a04ea85e3a72e294f3f8254
Jimmy_Olano <olano@ks7000.net.ve>
2014-10-17 11:46:53 -0430

    Agrego 'click.pcoo.rg' a 'correr_decoradores.py' y 'decora.py'

--- correr_decoradores.py
+++ correr_decoradores.py
@@ -1,9 +1,21 @@
+# -*- encoding=utf-8 -*-
+import click
 import decoradores
-valor = decoradores.logger(decoradores.sigma)
-valor ( 3, 5, 7)
 
-valor_dos = decoradores.sigma( 8, 2, 9)
-print valor_dos , "valor_dos"
-mi_valor = decoradores.logger(valor_dos)
-print mi_valor , "mi_valor"
+@click.command()
+@click.option( '--valor1', default=3, prompt='Introduzca el valor1 por favor', help='Valor1 es necesario')
+@click.option( '--valor2', default=5, prompt='Introduzca el valor2 por favor', help='Valor2 es necesario')
+@click.option( '--valor3', default=7, prompt='Introduzca el valor3 por favor', help='Valor3 es necesario')
+def muestra_valor( valor1, valor2, valor3):
+    valor = decoradores.logger(decoradores.sigma)
+    #valor ( 3, 5, 7)
+    valor ( valor1, valor2, valor3)
+
+if __name__ == '__main__':
+    muestra_valor()
+
+#valor_dos = decoradores.sigma( 8, 2, 9)
+#print valor_dos , "valor_dos"
+#mi_valor = decoradores.logger(valor_dos)
+#print mi_valor , "mi_valor"
 
--- decora.py
+++ decora.py
@@ -1,13 +1,21 @@
+# -*- encoding=utf-8 -*-
+import click
+
 def ver(fun):
     def _ver(*args, **kwargs):
-        print "Bienvenido", fun.__name__
+        #print u"Bienvenido", fun.__name__
         fun(*args, **kwargs)
-        print "Adios", fun.__name__
+        #print u"Adiós", fun.__name__
     return _ver
 
 @ver
+@click.command()
+@click.option('--x', default=1, prompt='Introduzca el valor de equis', help='Sumando equis')
+@click.option('--y', default=4, prompt='Introduzca el valor de ye', help='Sumando ye')
 def sumar(x,y):
     print x+y
 
-sumar(1, 4)
+if __name__ == '__main__':
+    sumar()
+    #sumar(1, 4)
 
